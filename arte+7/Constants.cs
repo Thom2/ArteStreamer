712df4f266d056cbd7c6f6c99af55d4e60d3b66a
Thom2 <oospamoo@gmx.de>
2019-11-16 01:11:08 +0100

    g

--- arte+7/Constants.cs
+++ arte+7/Constants.cs
@@ -5,9 +5,9 @@ namespace arte_7
     {
         public static bool IS_LOCAL = false;
 
-        public static string URL_VIDEO_MAIN = "http://videos.arte.tv";
-        public static string URL_VIDEO_PAGE = "http://videos.arte.tv/de/videos?page={0}";
-        public static string LOCAL_VIDEO_LOCATION = "../../../Test/videos{0}.htm";
+        public static string URL_VIDEO_MAIN = "https://www.arte.tv/";
+        public static string URL_VIDEO_PAGE = "https://www.arte.tv/de/videos/neueste-videos/";
+        public static string LOCAL_VIDEO_LOCATION = "../../../Test/sample_video_page.htm";
         public static string PLAYER_CLSID = "clsid:d27cdb6e-ae6d-11cf-96b8-444553540000";
 
     }
--- arte+7/VideoDecoder.cs
+++ arte+7/VideoDecoder.cs
@@ -9,63 +9,46 @@ namespace arte_7
             Video video = null;
             if (null != videoNode)
             {
-                HtmlNode titleNode = videoNode.SelectSingleNode("./h2/a");
+                HtmlNode titleNode = videoNode.SelectSingleNode("./div/a/div/img");
+                HtmlNode teaserCaption = videoNode.SelectSingleNode(".//div/a/div[@class='next-teaser__caption']");
                 // If there is no title node we don't have the complete video. Ignore it.
                 if (null != titleNode)
                 {
                     video = new Video();
-                    video.Title = VideoDecoder.GetTitle(titleNode);
-                    video.URL = VideoDecoder.GetURL(titleNode);
-                    video.TeaserText = VideoDecoder.GetTeaserText(videoNode);
-                    video.Thumbnail = VideoDecoder.GetThumbnail(videoNode);
-                    video.Duration = VideoDecoder.GetVideoDuration(videoNode);
-                    video.DisplayedAt = VideoDecoder.GetDisplayedAt(videoNode);
+                    video.Title = VideoDecoder.GetTitle(teaserCaption);
+                    video.URL = VideoDecoder.GetURL(videoNode);
+                    //video.TeaserText = VideoDecoder.GetTeaserText(videoNode);
+                    video.Thumbnail = VideoDecoder.GetThumbnail(titleNode);
+                    video.Duration = VideoDecoder.GetVideoDuration(teaserCaption);
+                    //video.DisplayedAt = VideoDecoder.GetDisplayedAt(videoNode);
                 }
             }
             return video;
         }
 
-        private static string GetTitle(HtmlNode titleNode)
+        private static string GetTitle(HtmlNode node)
         {
-            return HtmlEntity.DeEntitize(titleNode.InnerText);       
+            return HtmlEntity.DeEntitize(node.SelectSingleNode("./h3").InnerText);
         }
 
-        private static string GetURL(HtmlNode titleNode)
+        private static string GetURL(HtmlNode node)
         {
-            return GetNodeAttribute(titleNode, "href");           
+            return node.SelectSingleNode("./div/a").GetAttributeValue("href", "");
         }
 
         private static string GetTeaserText(HtmlNode videoNode)
         {
-            HtmlNode teaserTextNode = videoNode.SelectSingleNode("./div/div/p[@class='teaserText']");
-            if (null != teaserTextNode)
-            {
-                return HtmlEntity.DeEntitize(teaserTextNode.InnerText);
-            }
             return string.Empty;
         }
 
         private static string GetThumbnail(HtmlNode videoNode)
         {
-            HtmlNode thumbNailNode = videoNode.SelectSingleNode("./div/a/img");
-            if (null != thumbNailNode)
-            {
-                if (thumbNailNode.Attributes.Contains("src"))
-                {
-                    return Constants.URL_VIDEO_MAIN + thumbNailNode.Attributes["src"].Value;
-                }
-            }
-            return string.Empty;
+            return HtmlEntity.DeEntitize(videoNode.GetAttributeValue("src", ""));
         }
 
-        private static string GetVideoDuration(HtmlNode videoNode)
+        private static string GetVideoDuration(HtmlNode node)
         {
-            HtmlNode node = videoNode.SelectSingleNode("./div/div[@class='duration_thumbnail']");
-            if (null != node)
-            {
-                return node.InnerText;
-            }
-            return string.Empty;
+            return HtmlEntity.DeEntitize(node.SelectSingleNode("./p").InnerText);
         }
 
         private static string GetDisplayedAt(HtmlNode videoNode)
